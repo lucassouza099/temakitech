5c855fd2f64ad52c012bddc31a23e40d5234e3a1
lucassouza099 <ls.nascimento99@gmail.com>
2021-12-02 00:59:59 -0300

    t

--- database.py
+++ database.py
@@ -5,16 +5,16 @@ class database():
   def __init__(self):
      self.config = {
 
-      # 'user': 'w6orm2w7c0f9io2p',
-      # 'password': 'ndl4r94n3vhz73p6',
-      # 'host': 'yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-      # 'database': 'afx2fov6gpzbgr2g',
+      'user': 'w6orm2w7c0f9io2p',
+      'password': 'ndl4r94n3vhz73p6',
+      'host': 'yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      'database': 'afx2fov6gpzbgr2g',
 
 
-      'user': 'root',
-      'password': 'techit@2021',
-      'host': '127.0.0.1',
-      'database': 'temakitech',
+      # 'user': 'root',
+      # 'password': 'techit@2021',
+      # 'host': '127.0.0.1',
+      # 'database': 'temakitech',
 
       'raise_on_warnings': True
     }
